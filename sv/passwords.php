dcfc9d19ba415858d33cb0fb95843b6be7e76b29
fickler <christian@bundling.se>
2016-01-24 19:10:41 +0100

    Update passwords.php

--- sv/passwords.php
+++ sv/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'password' => 'Lösenord måste innehålla minst sex tecken och matcha varandra.',
-    'reset'    => 'Lösenordet har blivit återställt!',
+    'reset'    => 'Lösenordet har återställts!',
     'sent'     => 'Lösenordspåminnelse skickad!',
     'token'    => 'Koden för lösenordsåterställning är ogiltig.',
     'user'     => 'Vi kan inte hitta en användare med den e-postadressen.',
